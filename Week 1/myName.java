68399ef86246b0c62bad83e52adb6e19c8345dcc
Arpita_Hati <arpitahati830@gmail.com>
2024-09-04 19:54:28 +0530

    Delete Week 1/myName.java

--- Week 1/myName.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import copyright.copyright;
-public class myName {
-    public static void main(String[] args) {
-        copyright.main(args);
-        System.out.println("My name is Subha Saha");
-    }
-    
-}
